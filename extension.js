5b0d8f77da9656ab67b4bed7fa9b8ac57c9351ae
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2024-10-15 10:41:18 -0300

    agrega validacion para ejecutar el script de cada dp en determinadas urls

--- extension.js
+++ extension.js
@@ -1,22 +1,30 @@
 const DP_TEXT = {
-  SHAMING: "Mensaje generico de confirm shaming",
-  URGENCY: "Mensaje generico de fake urgency",
-  MISDIRECTION: "Mensaje generico de misdirection",
-  HIDDENCOST: "Mensaje generico de hidden cost"
+  SHAMING: 'Posible forma de persuasión',
+  URGENCY: 'Podría no ser cierto',
+  MISDIRECTION: 'Posible acción oculta',
+  HIDDENCOST: 'Posible precio oculto',
 };
+
+const DARK_PATTERNS = [
+  { name: 'HIDDEN_COSTS', check: hiddenCostScript, url: '/roomio' },
+  { name: 'MISDIRECTION', check: comprobarNodos, url: '/check_in' },
+  { name: 'FAKE_URGENCY', check: fakeUrgencyScript, url: '/ebook' },
+  { name: 'CONFIRMSHAMING', check: confirmShamingScript, url: '/car_rental' },
+];
+
 // Observer solo ejecuta el callback 1 segundos después de la ultima mutación
 // En páginas que cambian constantemente no sirve
 let previousURL = '';
 let timer;
-const observer = new MutationObserver(function(mutation){
+const observer = new MutationObserver(function (mutation) {
   if (timer) clearTimeout(timer);
   timer = setTimeout(() => {
-      hiddenCostScript();
-      //checkMisdirection();
-      comprobarNodos();
-      confirmShamingScript();
-      fakeUrgencyScript();
+    DARK_PATTERNS.forEach((dp) => {
+      if (window.location.pathname.includes(dp.url ?? '')) {
+        dp.check();
+      }
+    });
   }, 100);
 });
 
-observer.observe(document,{ childList: true, subtree: true });
+observer.observe(document, { childList: true, subtree: true });

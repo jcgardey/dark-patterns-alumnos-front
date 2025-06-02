36f6b012f9deb858a237f158e76a5bedf35de770
Facundo Pujol <facundopujolsc@gmail.com>
2025-06-02 16:14:33 -0300

    Guardo estados de las checkboxes

--- Extension/menu/pop-up.js
+++ Extension/menu/pop-up.js
@@ -1,8 +1,12 @@
 document.querySelectorAll('input[type=checkbox]').forEach((checkbox) => {
+	// Inicializar el estado del checkbox desde chrome.storage
+	chrome.storage.sync.get([checkbox.id], (result) => {
+		checkbox.checked = result[checkbox.id] || false;
+	});
     checkbox.addEventListener('change', () => {
       const state = checkbox.checked;
       const id = checkbox.id;
-      console.log(`${id} is now ${state}`);
+	  chrome.storage.sync.set({ [id]: state });
       // Puedes guardar esto con chrome.storage si quieres persistencia
     });
   });
